19058b92fa18f88ca6210a95f6589ffc5ba923a3
Logan Kimmel <kimmell@vmware.com>
2022-08-16 15:55:50 -0700

    Fix tests

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -21,11 +21,11 @@ class HelloControllerTest {
 
     @Test
     void index() throws Exception {
-        assertEquals("🏄🏻‍♂️ Greetings from Spring Boot + Tanzu on TAP! 🏄🏻‍♂️ ", controller.index());
+        assertEquals("🏄🏻‍♂️  Greetings from Spring Boot + Tanzu on TAP! 🏄🏻‍♂️", controller.index());
 
         mockMvc
             .perform(get("/"))
             .andExpect(status().isOk())
-            .andExpect(content().string("🏄🏻‍♂️  Greetings from Spring Boot + Tanzu on TAP! 🏄🏻‍♂️ "));
+            .andExpect(content().string("🏄🏻‍♂️  Greetings from Spring Boot + Tanzu on TAP! 🏄🏻‍♂️"));
     }
 }

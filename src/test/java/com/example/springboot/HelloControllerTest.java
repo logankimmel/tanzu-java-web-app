3e6d9a0da6b4101f8277f4e4cadf6839b14d887f
Logan Kimmel <kimmell@vmware.com>
2022-09-08 09:36:47 -0700

    added MC

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -21,11 +21,11 @@ class HelloControllerTest {
 
     @Test
     void index() throws Exception {
-        assertEquals("Greetings from Spring Boot + Tanzu on TAP!", controller.index());
+        assertEquals("Greetings MC from Spring Boot + Tanzu on TAP!", controller.index());
 
         mockMvc
             .perform(get("/"))
             .andExpect(status().isOk())
-            .andExpect(content().string("Greetings from Spring Boot + Tanzu on TAP!"));
+            .andExpect(content().string("Greetings MC from Spring Boot + Tanzu on TAP!"));
     }
 }
